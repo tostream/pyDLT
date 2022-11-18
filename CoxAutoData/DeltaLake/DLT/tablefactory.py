67e11943e2cda40be330c2aa4a1ddae1ef20a525
Ben <ben.fung@coxautoinc.com>
2022-11-18 14:35:23 +0000

    added loader to load transformation package

--- CoxAutoData/DeltaLake/DLT/tablefactory.py
+++ CoxAutoData/DeltaLake/DLT/tablefactory.py
@@ -1,25 +1,38 @@
 
 from typing import Any, Callable, Dict
 from abc import ABC, abstractmethod
-from ..transforms.transform import deltaLiveTable
 from pyspark.sql import dataframe
+from CoxAutoData.DeltaLake.transforms.transform import deltaLiveTable
+from CoxAutoData.DeltaLake.DLT import loader
 
 """ factory to prepare the transformation for a table"""
 
+
+table_creation_funcs: Dict[str, Callable[..., deltaLiveTable]] = {}
+
+def register(table_name: str, creator_fn: Callable[..., deltaLiveTable]) -> None:
+    """Register a table logic type."""
+    table_creation_funcs[table_name] = creator_fn
+
+def createTransform(arguments: Dict[str, Any]) -> deltaLiveTable:
+    #args_copy = arguments.copy()
+    transform = arguments.pop("transform")
+    try:
+        creator_func = table_creation_funcs[transform]
+    except KeyError:
+        raise ValueError(f"unknown character type {transform!r}") from None
+    return creator_func(transform)
+
 class deltaTables(ABC):
 
-    table_creation_funcs: Dict[str, Callable[..., deltaLiveTable]] = {}
 
     def __init__(self, CoxSpark, CoxDLT) -> None:
         self.CoxDLT = CoxDLT
         self.CoxSpark = CoxSpark
 
-    def register(self, table_name: str, creator_fn: Callable[..., deltaLiveTable]) -> None:
-        """Register a table logic type."""
-        self.table_creation_funcs[table_name] = creator_fn
-    
+
     @abstractmethod
-    def getTables(self, arguments: Dict[str, Any]) -> dataframe:
+    def getTables(self, arguments) -> dataframe:
         pass
 
 
@@ -39,26 +52,39 @@ class Bronze(deltaTables):
 class silver(deltaTables):
 
     def getTables(self, arguments: Dict[str, Any]) -> dataframe:
+        
+        # import the silver package
+        modules = arguments.pop('modules')
+        loader.import_module(modules)
+        #get ideal name
         tableName = arguments.pop('tableName')
-        transform = arguments.pop('transform')
         sourceTableName = arguments.pop('sourceTableName')
+        #get transformation mapping(dict)
+        transformName = arguments.pop('transform')
+        transform = createTransform(transformName)
         @self.CoxDLT.table(
             name = f"{tableName}"
         )
         def generatesilver():
-            return transform(self.CoxDLT.read(sourceTableName))
+            table = self.CoxDLT.read(sourceTableName)
+            return transform.transform(table)
 
 
 class gold(deltaTables):
 
     def getTables(self, arguments: Dict[str, Any]) -> dataframe:
+        modules = arguments.pop('modules')
+        loader.import_module(modules)
         tableName = arguments.pop('tableName')
-        transform = arguments.pop('transform')
+        transform = createTransform(arguments.pop('transform'))
         sourceTablesName = arguments.pop('sourceTableName')
-        for sourceTable in sourceTablesName:
-            sourceTablesName.update({sourceTable : self.CoxDLT.read(sourceTablesName[sourceTable])})
         @self.CoxDLT.table(
             name = f"{tableName}"
         )
         def generateGold():
-            return transform(**sourceTablesName)
+            para ={}
+            for sourceTable in sourceTablesName:
+                value=self.CoxDLT.read(sourceTablesName[sourceTable])
+                para[sourceTable]=value
+            #    sourceTablesName.update({sourceTable : self.CoxDLT.read(sourceTablesName[sourceTable])})
+            return transform.transform(**para)
