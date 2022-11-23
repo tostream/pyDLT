8592fd9b05e40492ff3ff57b9668e6341714e2de
Ben <ben.fung@coxautoinc.com>
2022-11-23 09:49:18 +0000

    refactored

--- CoxAutoData/DeltaLake/DLT/tablefactory.py
+++ CoxAutoData/DeltaLake/DLT/tablefactory.py
@@ -1,21 +1,20 @@
 
 from typing import Any, Callable, Dict
-from abc import ABC, abstractmethod
 from pyspark.sql import dataframe
 from CoxAutoData.DeltaLake.transforms.transform import deltaLiveTable
 from CoxAutoData.DeltaLake.DLT import loader
 
-""" factory to prepare the transformation for a table"""
-
+""" using plug-in architecture to seprated tranformation and pipeline"""
 
 table_creation_funcs: Dict[str, Callable[..., deltaLiveTable]] = {}
 
 def register(table_name: str, creator_fn: Callable[..., deltaLiveTable]) -> None:
-    """Register a table logic type."""
+    """Register a transformation."""
     table_creation_funcs[table_name] = creator_fn
 
 def createTransform(arguments: Dict[str, Any]) -> deltaLiveTable:
-    #args_copy = arguments.copy()
+    """get the transformation logic"""
+
     transform = arguments.pop("transform")
     try:
         creator_func = table_creation_funcs[transform]
@@ -23,68 +22,78 @@ def createTransform(arguments: Dict[str, Any]) -> deltaLiveTable:
         raise ValueError(f"unknown character type {transform!r}") from None
     return creator_func(transform)
 
-class deltaTables(ABC):
+class deltaTables():
+    "Cox internal delta live table framework"
 
+    _rawTablesList = {}
+    _idealTablesList = {}
 
     def __init__(self, CoxSpark, CoxDLT) -> None:
         self.CoxDLT = CoxDLT
         self.CoxSpark = CoxSpark
 
+    @property
+    def rawTablesList(self):
+        return self._rawTablesList
+    
+    @rawTablesList.setter
+    def rawTablesList(self, rawTablesList: Dict):
+        self._rawTablesList = rawTablesList
 
-    @abstractmethod
-    def getTables(self, arguments) -> dataframe:
-        pass
+    @property
+    def idealTablesList(self):
+        return self._idealTablesList
+    
+    @idealTablesList.setter
+    def idealTablesList(self, rawTablesList: Dict):
+        self._idealTablesList = rawTablesList
 
+    def importModule(self, module: str):
+        """ loaded the transformation logic into the pipeline"""
+        if module is not None:
+            loader.import_module(module)
 
-class Bronze(deltaTables):
+    def praseArguments(self, arguments: Dict[str, Any]) -> Dict:
+        res = {}
+        # todo: add checking/validation
+        res['tableName'] = arguments.pop('tableName',None)
+        res['fileFormat'] = arguments.pop('fileFormat',None)
+        res['sourceTableName'] = arguments.pop('sourceTableName',None)
+        res['transformName'] = arguments.pop('transform',None)
+        res['modules'] = arguments.pop('modules',None)
+        return res
 
-    def getTables(self, arguments: Dict[str, Any]) -> dataframe:
-        tableName = arguments.pop('tableName')
-        fileFormat = arguments.pop('fileFormat')
-        sourceTableName = arguments.pop('sourceTableName')
-        @self.CoxDLT.table(
-            name = f"{tableName}"
-        )
-        def generateBronze():
-            return self.CoxSpark.read.format(fileFormat).load(sourceTableName)
-
-
-class silver(deltaTables):
-
-    def getTables(self, arguments: Dict[str, Any]) -> dataframe:
+    def getRawTables(self, arguments: Dict[str, Any]) -> dataframe:
         
-        # import the silver package
-        modules = arguments.pop('modules')
-        loader.import_module(modules)
-        #get ideal name
-        tableName = arguments.pop('tableName')
-        sourceTableName = arguments.pop('sourceTableName')
-        #get transformation mapping(dict)
-        transformName = arguments.pop('transform')
-        transform = createTransform(transformName)
+        para = self.praseArguments(arguments)
+        return self.__generateTable(para['tableName'],
+            self.CoxSpark.read.format(para['fileFormat']).load(),
+            para['sourceTableName'])
+
+    def getIdealTables(self, arguments: Dict[str, Any]) -> dataframe:
+        
+        para = self.praseArguments(arguments)
+        
+        self.importModule(para['modules'])
+        sourceTablesName = para['sourceTableName']
+        transform = createTransform(para['transformName'])
+        para = {arguments['sourceTableName'] : self.CoxDLT.read(sourceTablesName)}
+        return self.__generateTable(para['tableName'], transform.transform ,para)
+
+    def getBOTables(self, arguments: Dict[str, Any]) -> dataframe:
+
+        para = self.praseArguments(arguments)
+        
+        self.importModule(para['modules'])
+        sourceTablesName = para['sourceTableName']
+        transform = createTransform(para['transformName'])
+        sourceTables = {k: self.CoxDLT.read(v) for k, v in sourceTablesName.items()}
+        return self.__generateTable(para['tableName'], transform.transform ,sourceTables)
+
+    def __generateTable(self, tableName, transform, sourceTables) -> dataframe:
         @self.CoxDLT.table(
-            name = f"{tableName}"
+            name = tableName
         )
-        def generatesilver():
-            table = self.CoxDLT.read(sourceTableName)
-            return transform.transform(table)
-
-
-class gold(deltaTables):
-
-    def getTables(self, arguments: Dict[str, Any]) -> dataframe:
-        modules = arguments.pop('modules')
-        loader.import_module(modules)
-        tableName = arguments.pop('tableName')
-        transform = createTransform(arguments.pop('transform'))
-        sourceTablesName = arguments.pop('sourceTableName')
-        @self.CoxDLT.table(
-            name = f"{tableName}"
-        )
-        def generateGold():
-            para ={}
-            for sourceTable in sourceTablesName:
-                value=self.CoxDLT.read(sourceTablesName[sourceTable])
-                para[sourceTable]=value
-            #    sourceTablesName.update({sourceTable : self.CoxDLT.read(sourceTablesName[sourceTable])})
-            return transform.transform(**para)
+        def generate():
+            return transform(**sourceTables)
+    
